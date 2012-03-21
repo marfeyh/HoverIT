a7f766ebc2096dafab8273e0eddbe727190bae1e
Yavor Paunov <guspauya@student.gu.se>
2012-03-21 15:03:14 +0100

    Comments added to example.

--- src/computer/bluetooth_test.c
+++ src/computer/bluetooth_test.c
@@ -32,20 +32,29 @@ int main(int argc, char **argv)
         exit(1);
     }
     printf("sock %d \n", sock);
-
+    // Max time to look for devices. (mult. by 1.28 sec)
     len  = 8;
+    // Max devices to look for:
     max_rsp = 255;
+    // Flushes the cache of previously detected devices.
     flags = IREQ_CACHE_FLUSH;
+    // Allocates memory for the returned devices.
     ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
     
+    // Look for devices and assign to pointer ii. Return number of found devices.
     num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
+    // If number of found devices is less than 0, crash with error.
     if( num_rsp < 0 ) perror("hci_inquiry");
-
+    // Loop through found devices.
     for (i = 0; i < num_rsp; i++) {
+      // Convert the bdaddr_t structure of the device to a readable string of the format XX:XX:XX:XX:XX:XX
         ba2str(&(ii+i)->bdaddr, addr);
+	
         memset(name, 0, sizeof(name));
+	// Get the user friendly name of the device.
         if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name), 
             name, 0) < 0)
+	  // If name is empty, set to [unknown]
         strcpy(name, "[unknown]");
         printf("%d. %s  %s\n", i, addr, name);
     }
