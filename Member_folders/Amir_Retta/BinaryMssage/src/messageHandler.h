ac78d3dadc166860f94a59d00507ec072669c607
Amir Almasi <amir.fireflame@gmail.com>
2012-03-28 15:50:03 +0200

    Added a module. This module handles the binary messages based on the protocol which is descussed

---
+++ Member_folders/Amir_Retta/BinaryMssage/src/messageHandler.h
@@ -0,0 +1,19 @@
+/*
+ * messageHandler.h
+ *
+ *  Created on: Mar 28, 2012
+ *      Author: amir
+ */
+
+#ifndef MESSAGEHANDLER_H_
+#define MESSAGEHANDLER_H_
+unsigned char parseBinary(unsigned char message);
+unsigned char findMessage(unsigned char* message);
+unsigned char create_fan_forward_speed(unsigned char* message);
+unsigned char create_fan_hovering_speed(unsigned char* message);
+unsigned char create_ruder_direction(unsigned char* message);
+unsigned char create_hovercraft_speed(unsigned char* message);
+unsigned char create_hovercraft_pressure(unsigned char* message);
+unsigned char create_battery_level(unsigned char* message);
+unsigned char check_number_limit(unsigned char* message);
+#endif /* MESSAGEHANDLER_H_ */
