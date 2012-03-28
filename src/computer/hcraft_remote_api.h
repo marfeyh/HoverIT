57fe5e20e612e4494a93da7587d259be228c6f16
Evelina Vorobyeva <effochka@gmail.com>
2012-03-28 15:05:03 +0200

    Makefile added, integrated

--- src/computer/hcraft_remote_api.h
+++ src/computer/hcraft_remote_api.h
@@ -2,12 +2,5 @@
 #define HCRAFT_REMOTE_API_H
 int hc_connect();
 int hc_disconnect();
-
 int set_rudder(unsigned char value);
-int set_hover_speed(unsigned char value);
-int set_move_speed(unsigned char value);
-int get_speed(unsigned char *value);
-int get_battery(unsigned char *value);
-int get_pressure(unsigned char *value);
-
 #endif
