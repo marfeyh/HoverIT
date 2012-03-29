b2815bb5c1187d21bec29d6816c9ef2c335dcec6
Seyed Ehsan Mohajerani <gusmohase@student.gu.se>
2012-03-29 10:05:16 +0200

    Removed code accidently commited to master

--- src/embedded/propulsion/pro_api.h
+++
@@ -1,6 +0,0 @@
-int increase_speed();
-int speedLevel;
-int decrease_speed();
-int get_fan_level();
-int newprospeed;
-int getSpeedLevel();
--- src/embedded/propulsion/propelsion.h
+++
@@ -1,6 +0,0 @@
-int level;
-int getSpeedLevel();
-int getSpeedLevel();
-int stop_pro_fan();
-void change_pro_speed(int level);
-int stop_pro_fan();
