c81ed7a6fa2a9e5df7ff13f410759d7dba213d92
Amir Almasi <amir.fireflame@gmail.com>
2012-03-29 16:59:59 +0200

    Changes in the create functions. Also the commeting is done

---
+++ Member_folders/Amir_Retta/BinaryMessage/src/messageHandler.c
@@ -0,0 +1,147 @@
+/*
+ * massageHandler.c
+ *
+ *  Created on: Mar 28, 2012
+ */
+#include<stdio.h>
+#include "messageHandler.h"
+
+#define Fan_Forward_Speed 0;
+#define Fan_Hovering_Speed 1;
+#define Ruder_Direction 2;
+#define Hovercraft_Speed 3;
+#define Hovercraft_Pressure 4;
+#define Battery_Level 5;
+#define Connection 6;
+
+
+/**
+ * A function which is called when there is binary received on serial.
+ * When binary argument is based on protocol the return of the function is: 0 - 5
+ * When binary argument is not based on anything on protocol the
+ * return of the function is 255
+ *
+ */
+unsigned char parseBinary(unsigned char message) {
+//	printf("The message is: %x\n", message);
+	return findMessage(&message);
+}
+
+/**
+ * A function to find the type of the message based on the received binary
+ * The function returns 0 - 5 if the binary message is based on the protocol
+ * The function returns 255 when the binary message is not based on the protocol.
+ */
+unsigned char findMessage(unsigned char* message) {
+	switch ((*message & (15 << 4)) >> 4) {
+	case 0:
+		return Fan_Forward_Speed;
+		break;
+	case 1:
+		return Fan_Hovering_Speed;
+		break;
+	case 2:
+		return Ruder_Direction;
+		break;
+	case 3:
+		return Hovercraft_Speed;
+		break;
+	case 4:
+		return Hovercraft_Pressure;
+		break;
+	case 5:
+		return Battery_Level;
+		break;
+	}
+	return 255;
+}
+
+/**
+ * A function to create fan forward speed based on protocol
+ * Function returns 255 if the value is bigger the limit
+ * Function returns the fan forward speed binary message with the value
+ */
+unsigned char create_fan_forward_speed(unsigned char* message) {
+	if (check_number_limit(message) == 255){
+		return 255;
+	}
+	return *message;
+}
+
+
+/**
+ * A function to create fan hovering speed based on protocol
+ * Function returns 255 if the value is bigger the limit
+ * Function returns the fan hovering speed binary message with the value
+ */
+unsigned char create_fan_hovering_speed(unsigned char* message) {
+	if (check_number_limit(message) == 255){
+		return 255;
+	}
+	return (1<<4) | *message;
+}
+
+
+/**
+ * A function to create ruder direction based on protocol
+ * Function returns 255 if the value is bigger the limit
+ * Function returns the ruder direction binary message with the value
+ */
+unsigned char create_ruder_direction(unsigned char* message) {
+	if (check_number_limit(message) == 255){
+			return 255;
+		}
+		return (1<<5) | *message;
+}
+
+
+/**
+ * A function to create hover craft speed based on protocol
+ * Function returns 255 if the value is bigger the limit
+ * Function returns the ruder direction binary message with the value
+ */
+unsigned char create_hovercraft_speed(unsigned char* message) {
+	if (check_number_limit(message) == 255){
+			return 255;
+		}
+		return (3<<4) | *message;
+}
+
+
+/**
+ * A function to create hover craft pressure based on protocol
+ * Function returns 255 if the value is bigger the limit
+ * Function returns the pressure binary message with the value
+ */
+unsigned char create_hovercraft_pressure(unsigned char* message) {
+	if (check_number_limit(message) == 255){
+			return 255;
+		}
+		return (1<<6) | *message;
+}
+
+/**
+ * A function to create battery level based on protocol
+ * Function returns 255 if the value is bigger the limit
+ * Function returns the battery level binary message with the value
+ */
+unsigned char create_battery_level(unsigned char* message) {
+	if (check_number_limit(message) == 255){
+			return 255;
+		}
+		return (5<<4) | *message;
+}
+
+/**
+ * A function to check the limit of the value of the message
+ * The limit of the message is 15 ( 4 bits )
+ * Function returns 255 if the value is bigger the limit
+ * Function returns 1 if the message is in the range
+ */
+unsigned char check_number_limit(unsigned char* message){
+	if ((*message > 15) || *message<0){
+		return 255;
+	}
+	return 1;
+}
+
