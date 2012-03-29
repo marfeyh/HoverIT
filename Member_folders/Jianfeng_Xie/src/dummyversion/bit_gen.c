c0de1f981141a5d7ab61aaf0e502ebfbafee89d2
JianfengXie <feng.zec@gmail.com>
2012-03-29 14:29:24 +0200

    add source code for dummy version

---
+++ Member_folders/Jianfeng_Xie/src/dummyversion/bit_gen.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int rando(){
+	int a;
+	srand((unsigned)time(NULL));
+	a = rand()%255;
+	return a;
+}
+
+int extract(){
+	int b,bs,c,cs,d,ds,e,es;
+	int x = rando();
+	int y;
+
+		printf("%d\n",x);
+			b=x&7;
+			scanf("%d",&bs);
+			b=bs;
+			printf("%d\n",b);
+
+			c=(x>>3)&3;
+			scanf("%d",&cs);
+			c=cs;
+			printf("%d\n",c);
+
+			d=(x>>5)&3;
+			scanf("%d",&ds);
+			d=ds;
+			printf("%d\n",d);
+
+			e=(x>>7)&0;
+			scanf("%d",&es);
+			e=es;
+			printf("%d\n",e);
+
+		y = (b|(c<<3)|(d<<5)|(e<<7));
+
+		printf("%d\n",y);
+
+	return 0;
+}
+
+void loop(){
+	while (1) {
+		printf("number: %d \n",rando());
+		sleep(1);
+		
+	}
+}
+
+/*void main(){
+
+	//printf("%d\n",rando());
+	extract();
+}*/	
