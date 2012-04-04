07c818ca80b64fb5071791c0a222ca995f10214c
Eva-Lisa Kedborn <guskedev@student.gu.se>
2012-04-04 11:18:47 +0200

    hover api files added

---
+++ src/embedded/hover_api/main.c
@@ -0,0 +1,13 @@
+/*
+Author: Eva-Lisa Kedborn, Jing Liu
+Date: 2012-03-19
+Description: the main to execute pressure_loop
+*/
+
+#include <pressure_check.h>
+
+int main(void) {
+  start_sensor_reading();
+
+  return 0;
+}
