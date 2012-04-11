ec008828d79e04f6fd523d37f21583a8910a70e5
unknown <Mo@Mo-PC.(none)>
2012-04-11 13:32:53 +0200

    The files controller.c and Theta-API.h were updated with new comments following the provided standards

--- src/embedded/Scheduler/Theta-API.h
+++ src/embedded/Scheduler/Theta-API.h
@@ -1,5 +1,13 @@
-#ifndef __Theta-API_h_
-#define __Theta-API_h_
-void control();
-void putJobInQueue(struct Job job);
+/** 
+ *  \file			Theta-API.h	
+ *	\details		This module could be included wherever either of the functions 
+ *					"control" or "putJobInQueue" are invoked.
+ *	\date			09-04-2012 
+ *	\version		0.1
+ *	\author 		Mozhan Soltani
+ */
+#if !defined Theta-API_h
+ #define Theta-API_h
+	void control();
+	void putJobInQueue(struct Job job);
 #endif
