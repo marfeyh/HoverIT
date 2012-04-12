ae036befc6453a1df2752d5e6f11b64bcef45e5e
Amir Almasi <amir.fireflame@gmail.com>
2012-04-12 15:14:07 +0200

    added the lastest changes

---
+++ bin/include/bluetooth_controler.h
@@ -0,0 +1,15 @@
+/*
+ * This header file is for internal functions
+ * bluetooth_controler.h
+ *
+ *  Created on: Apr 9, 2012
+ *      Author: amir
+ */
+
+#ifndef BLUETOOTH_CONTROLER_H_
+#define BLUETOOTH_CONTROLER_H_
+
+void send_serial_string(char* string);
+void send_serial_binary(unsigned char* binary);
+
+#endif /* BLUETOOTH_CONTROLER_H_ */
