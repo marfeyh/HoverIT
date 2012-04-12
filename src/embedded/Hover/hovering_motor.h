ccfc2b67bf0927dc60edf6ff4c1bd541f8129661
Seyed Ehsan Mohajerani <gusmohase@student.gu.se>
2012-04-12 09:50:04 +0200

    some changes

---
+++ src/embedded/Hover/hovering_motor.h
@@ -0,0 +1,45 @@
+/* 
+ * @ Module name:  hovering_motor.h
+ * @ Description:  Header file for controlling hovering motor by
+ *                 hovering_motor.c hover_api.c
+ *
+ * @ Author names : Seyed Ehsan Mohajerani, Navid Amiriarshad
+ * @ Release      : 20 March 2012
+ * @ Version      : 1.0
+ * @ Refrences    : Arduino.cc, www.sandklef
+ * dreamincode.net/forums/topic/34861-functions-stored-in-structure
+ * Turnigy_Plush_and_Sentry_ESC user manual
+ */
+
+
+#define BLINK_PIN 11
+#define BLINKER(a) { int i; pinMode(BLINK_PIN,OUTPUT);for (i=0;i<10;i++) { digitalWrite(BLINK_PIN,HIGH);delay(a);digitalWrite(BLINK_PIN,LOW);delay(2*a); } delay(1000); }
+
+
+/* Make sure this header file is included one time */
+#ifndef HOVERING_MOTOR_H
+	#define HOVERING_MOTOR_H
+
+	/* Struct to be used in hover_loop */
+	typedef struct {
+		int (*func1)(int);
+		void (*func2)(long unsigned int);
+		int level;
+		int delay_time;
+	} FUNCS;
+
+	/* Function prototypes */
+	/* Initialize Arduino */
+	int ard_init(int using_pin);
+	/* Function that gets arguments for start or stop the motor */
+	int hover_func(int (*func)(int), int using_pin);
+	/* Fucntion that gets arguments for change the speed of motor */	
+	int hover_change(int (*climb_decesnd)(int,int),
+	 int using_pin, int level);
+	/* Loop for motor */
+	int hover_loop(FUNCS funcs_in, int using_pin,
+	 int throttle_stick_level);
+	/* Set the Arduino active pin for using */	
+	int pin_program(int using_pin, int level);
+
+	#endif
