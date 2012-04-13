eab008be13a1d83f59f3dbc5d091be19b7cbbc89
Mozhan <mozhan.soltani@gmail.com>
2012-04-13 18:04:53 +0200

    job.h and jobpriority.h were updated

--- src/embedded/Scheduler/jobpriority.h
+++ src/embedded/Scheduler/jobpriority.h
@@ -1,11 +1,22 @@
-/**
-Author: Dmitry Igoshin
-Job priorities
-**/
+/** 
+ *  \file			jobpriority.h	
+ *	\version		0.1
+ *	\author 		Dmitri Igoshin
+ *	\attention 		Sections added/modified are as follow: 
+ *                  - The initial enum PRIORITY was provided.
+ *  \n
+ *	\date			13-04-2012  
+ *	\version		0.2
+ *	\author 		Mozhan Soltani
+ *	\attention 		Sections added/modified are as follow: 
+ *                  - The prefix "PRIO_" was added to all fields of the enum due to a reported conflict with arduino library.
+ *					- Comment was modified.
+ *					- Guards were modified to foloow the provided standard.
+ */
 #pragma once
-#ifndef __jobpriority_h_
-#define __jobpriority_h_
+#if !defined jobpriority_h
+#define jobpriority_h
 
-enum PRIORITY {LOW=10,MEDIUM=20,HIGH=30,RIGHT_NOW=40};
+enum PRIORITY {PRIO_LOW=10,PRIO_MEDIUM=20,PRIO_HIGH=30,PRIO_RIGHT_NOW=40};
 
 #endif
