eb9313fa65cc9bf054d1ec0919af8ef3e6812a74
monzavi <id.monzavi@gmail.com>
2012-04-16 14:16:24 +0200

    this is the Arduino code-first implementation

---
+++ src/embedded/Wifi-Arduino/libraries/WiFlySerial/Examples/WebTimeA1/Credentials.h
@@ -0,0 +1,9 @@
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
+// Wifi parameters
+char passphrase[] = "12345678";
+char ssid[] = "massih";
+char ntp_server[] = "nist1-la.ustiming.org"; // or your favorite ntp server.
+
+#endif
