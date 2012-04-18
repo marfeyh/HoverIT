7156ca893b6c3850f7ca90852128a278035f672b
Paulius Vysniauskas <gusvysnpa@student.gu.se>
2012-04-18 11:57:10 +0200

    Added the latest version after git problems

---
+++ src/embedded/Rudder_control/src/main.c
@@ -0,0 +1,16 @@
+/*
+ * Author: Rashid Darwish, David Giorgidze, Zarif Jawad, Paulius Vysniauskas.
+ * Date: 2012-04-04.
+ * Description: main function in C, required to initialize RuddarControll
+ * object and begin execution.
+ *
+ */
+
+#include "steering.h"
+
+int main(void)
+{
+  control_rudder(1);
+  return 0;	
+}
+
