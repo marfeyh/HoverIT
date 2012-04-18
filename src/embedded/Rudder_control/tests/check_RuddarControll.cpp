47c356ec8ef680471d55f541adcc75752b0c9f6b
Paulius Vysniauskas <gusvysnpa@student.gu.se>
2012-04-18 12:49:12 +0200

    Latest version with folder structure changed

---
+++ src/embedded/Rudder_control/tests/check_RuddarControll.cpp
@@ -0,0 +1,25 @@
+/*
+ * Author: Paulius Vysniauskas, David Giorgidze.
+ * Date: 2012-04-04.
+ * Test case implementation.
+ *
+ */
+
+#include <check.h>
+#include "../RuddarControll.h"
+
+START_TEST (test_one)
+{
+  RuddarControll *r = new RuddarControll();
+  init();
+  fail_unless(r->setupRuddar()== 0, "FAILED: Could not setup rudder");
+}
+END_TEST
+
+Suite * calc_suite(void) {
+  Suite *s = suite_create("Ruddar");
+  TCase *tc = tcase_create("First_case");
+  tcase_add_test(tc,test_one);
+  suite_add_tcase(s,tc);
+  return s;
+}
