0d557de04608ba375f6246e3f479b8fca3173236
Peter <peter@myllykoski.se>
2012-04-18 17:10:12 +0200

    Files for building the whole hovercraft project added

---
+++ src/embedded/Hovercraft/main.c
@@ -0,0 +1,16 @@
+#include <Theta-API.h>
+#include <API_bluetooth_controler.h>
+
+int main(void) {
+  init();
+  bluetooth_serial_setup();
+  //start_propulsion_fan();
+  //start_hover();
+  //control_rudder(0);
+
+  for(;;) {
+    control();
+  }
+  return 0;
+}
+
