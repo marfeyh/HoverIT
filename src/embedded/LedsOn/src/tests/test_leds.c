db2f17bd52cd1b991550a8b6d1dc1015387ebc03
Nahid Vafaie <nahid@student.gu.se>
2012-04-18 10:55:59 +0200

    added more test cases to test the function.

--- src/embedded/LedsOn/src/tests/test_leds.c
+++ src/embedded/LedsOn/src/tests/test_leds.c
@@ -14,7 +14,7 @@
 
 
 void setup (void) {
-
+  init();
 
 }
 
@@ -22,7 +22,7 @@ void teardown (void) {
 
 }
 
-START_TEST(test_addition) {
+START_TEST(test_init_one) {
 
   init_leds();
 	fail_unless(digitalRead(ledPin4) == HIGH);
@@ -30,6 +30,33 @@ START_TEST(test_addition) {
 
 }END_TEST
 
+START_TEST(test_loop) {
+
+  run_leds();
+	fail_unless(digitalRead(ledPin4) == LOW);
+	fail_unless(digitalRead(ledPin3) == LOW);
+    fail_unless(digitalRead(ledPin2) == LOW);
+    fail_unless(digitalRead(ledPin1) == LOW);
+
+}END_TEST
+
+
+START_TEST(test_init_two) {
+
+  init_leds();
+	fail_unless(digitalRead(ledPin4) == HIGH);
+	fail_unless(digitalRead(ledPin2) == LOW);
+
+
+}END_TEST
+
+
+START_TEST(test_init_third) {
+  init_leds();
+  fail_unless(digitalRead(ledPin4) == HIGH);
+  fail_unless(digitalRead(ledPin1) == LOW);
+
+}END_TEST
 
 
 Suite * leds_suite(void) {
@@ -37,7 +64,11 @@ Suite * leds_suite(void) {
 
   TCase *tc = tcase_create("Core");
   tcase_add_checked_fixture(tc,setup,teardown);
-  tcase_add_test(tc,test_addition);
+  tcase_add_test(tc,test_init_one);
+  tcase_add_test(tc,test_init_two);
+  tcase_add_test(tc,test_init_third);
+  tcase_add_test(tc,test_loop);
+  tcase_set_timeout(tc,0);
   suite_add_tcase(s,tc);
   return s;
 }
