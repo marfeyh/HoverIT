cb95cc1c390784b8096b4bd14a0c61ef5125e4b8
David Giorgidze <gusgiorda@student.gu.se>
2012-04-20 14:48:31 +0200

    Adding GPL License

--- src/embedded/Rudder_control/RuddarControll.cpp
+++ src/embedded/Rudder_control/RuddarControll.cpp
@@ -1,3 +1,23 @@
+/*
+ * RuddarControll.cpp
+ * This file is part of Rudder Control
+ *
+ * Copyright (C) 2012 - Epsilon
+ *
+ * Rudder Control is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Rudder Control is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Rudder Control. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * @file RuddarControll.cpp
  * @brief Description: Ruddar Control API for hovercraft. Which provides
--- src/embedded/Rudder_control/RuddarControll.h
+++ src/embedded/Rudder_control/RuddarControll.h
@@ -1,3 +1,23 @@
+/*
+ * RuddarControll.h
+ * This file is part of Rudder Control
+ *
+ * Copyright (C) 2012 - Epsilon
+ *
+ * Rudder Controll is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Rudder Controll is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Rudder Controll. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * @file RuddarControll.h
  * @brief Description: Header file for Ruddar Control API for hovercraft.
