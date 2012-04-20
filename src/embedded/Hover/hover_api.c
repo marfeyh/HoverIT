e30e176c4b70aa46b7091653d6958881ba2b859b
Jing Liu <gusjingli@student.gu.se>
2012-04-20 13:57:44 +0200

    hover_api comment changed according to doxygen syntax

--- src/embedded/Hover/hover_api.c
+++ src/embedded/Hover/hover_api.c
@@ -1,8 +1,9 @@
-/* 
-*@Author: Eva-Lisa Kedborn, Jing Liu
-*@Date: 2012-03-28
-*@Description: API for controling hovering fan
-*/
+/**
+ *@file hover_api.c
+ *@Author  Eva-Lisa Kedborn, Jing Liu
+ *@date 2012-03-28
+ *@brief API for controling hovering fan
+ */
 
 #include <stdlib.h>
 #include <hover_api.h>
@@ -13,37 +14,58 @@
 
 #define HOVER_LEVEL 10
 
-int start_hover() {         /* start the hovering fan */ 
+/**
+ * @brief start the hovring fan
+ */
+int start_hover() {
   initialize(USING_PIN);
   return 0;
 }
 
-int stop_hover() {           /* stop the fan */
+/**
+ * @brief stop the fan
+ */
+int stop_hover() {          
   hover_func(stop, USING_PIN);
   return 0;
 }
 
-int hover_min() {   /* minimize the speed */
+/**
+ * @brief minimize the speed
+ */
+int hover_min() {
   hover_func(normal, USING_PIN);
   return 0;
 }
 
-int hover_max() {   /* maximize the speed */
+/**
+ * @brief maximize the speed
+ */
+int hover_max() {
   hover_func(turbo, USING_PIN);
   return 0;
 }
 
-int decrease_hover() {       /* decrease the speed */
+/**
+ * @brief decrease the speed
+ */
+int decrease_hover() {       
   hover_change(decrease, USING_PIN,HOVER_LEVEL);
   return 0;
 }
 
-int increase_hover() {       /* increase the speed */
+/**
+ * @brief increase the speed
+ */
+int increase_hover() {
   hover_change(increase, USING_PIN, HOVER_LEVEL);
   return 0;
 }
 
-int autocontrol() {         /* let the fan speed be cotrolled by pressure sensor			       automatically */
+/**
+ * @brief fan speed cotrolled by pressure sensor automatically
+ */
+int autocontrol() {       
   start_sensor_reading();
   return 0;
 }
