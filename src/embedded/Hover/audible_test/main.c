55d322a58cee34fe901531bd6259b539be04f8b4
navid amiriarshad <amirinavid83@gmail.com>
2012-04-20 11:59:06 +0200

    adding audible test files

---
+++ src/embedded/Hover/audible_test/main.c
@@ -0,0 +1,20 @@
+/*
+Author: Eva-Lisa Kedborn, Jing Liu
+Date: 2012-03-19
+Description: the main to execute pressure_loop
+*/
+
+/* Includes */
+#include <audible_test.h>
+#include "Arduino.h"
+
+int main(void) {
+        //init();
+	//for (;;) {	
+init();
+	
+loop();
+//BLINKER(500);
+//}
+	return 0;
+}
