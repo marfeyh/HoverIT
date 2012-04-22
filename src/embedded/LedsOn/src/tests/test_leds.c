15d9f673fa5c46ec82c6633d374d89f4c36a8176
Nahid Vafaie <nahid@student.gu.se>
2012-04-22 19:25:37 +0200

    more tests added.

--- src/embedded/LedsOn/src/tests/test_leds.c
+++ src/embedded/LedsOn/src/tests/test_leds.c
@@ -66,7 +66,60 @@ START_TEST(test_loop_half1) {
 
 }END_TEST
 
+START_TEST(test_loop_half2) {
+  init_leds();
 
+  display_percentage(54);
+	fail_unless(digitalRead(ledPin1) == LOW);
+	fail_unless(digitalRead(ledPin2) == HIGH);
+    fail_unless(digitalRead(ledPin3) == HIGH);
+    fail_unless(digitalRead(ledPin4) == HIGH);
+
+}END_TEST
+
+START_TEST(test_loop_lesshalf1) {
+  init_leds();
+
+  display_percentage(53);
+	fail_unless(digitalRead(ledPin1) == LOW);
+	fail_unless(digitalRead(ledPin2) == LOW);
+    fail_unless(digitalRead(ledPin3) == HIGH);
+    fail_unless(digitalRead(ledPin4) == HIGH);
+
+}END_TEST
+
+START_TEST(test_loop_lesshalf2) {
+  init_leds();
+
+  display_percentage(28);
+	fail_unless(digitalRead(ledPin1) == LOW);
+	fail_unless(digitalRead(ledPin2) == LOW);
+    fail_unless(digitalRead(ledPin3) == HIGH);
+    fail_unless(digitalRead(ledPin4) == HIGH);
+
+}END_TEST
+
+START_TEST(test_loop_empty1) {
+  init_leds();
+
+  display_percentage(27);
+	fail_unless(digitalRead(ledPin1) == LOW);
+	fail_unless(digitalRead(ledPin2) == LOW);
+    fail_unless(digitalRead(ledPin3) == LOW);
+    fail_unless(digitalRead(ledPin4) == LOW);
+
+}END_TEST
+
+START_TEST(test_loop_empty2) {
+  init_leds();
+
+  display_percentage(1);
+	fail_unless(digitalRead(ledPin1) == LOW);
+	fail_unless(digitalRead(ledPin2) == LOW);
+    fail_unless(digitalRead(ledPin3) == LOW);
+    fail_unless(digitalRead(ledPin4) == LOW);
+
+}END_TEST
 
 
 Suite * leds_suite(void) {
@@ -75,9 +128,14 @@ Suite * leds_suite(void) {
   TCase *tc = tcase_create("Core");
   tcase_add_checked_fixture(tc,setup,teardown);
   tcase_add_test(tc,test_init);
-   tcase_add_test(tc,test_loop_full);
-   tcase_add_test(tc,test_loop_full2);
-    tcase_add_test(tc,test_loop_half1);
+  tcase_add_test(tc,test_loop_full);
+  tcase_add_test(tc,test_loop_full2);
+  tcase_add_test(tc,test_loop_half1);
+  tcase_add_test(tc,test_loop_half2);
+  tcase_add_test(tc,test_loop_lesshalf1);
+  tcase_add_test(tc,test_loop_lesshalf2);
+  tcase_add_test(tc,test_loop_empty1);
+  tcase_add_test(tc,test_loop_empty2);
   tcase_set_timeout(tc,0);
   suite_add_tcase(s,tc);
   return s;
