da72fad969512a33ed28d97eaff7ed91a254c47f
Nahid Vafaie <nahid@student.gu.se>
2012-04-22 19:12:42 +0200

    Second version of test cases added

--- src/embedded/LedsOn/src/tests/test_leds.c
+++ src/embedded/LedsOn/src/tests/test_leds.c
@@ -2,8 +2,8 @@
  * @ Module name:  checkmain.c
  * @ Description:  Contains the functions that tests the leds be on and off
  * @ Author names : Nahid Vafaie
- * @ Release      : 16 April 2012
- * @ Version      : 1
+ * @ Release      : 20 April 2012
+ * @ Version      : 2
  * @ Refrences    : Arduino.cc, sandklef.com, Check manual
  */
 
@@ -22,41 +22,51 @@ void teardown (void) {
 
 }
 
-START_TEST(test_init_one) {
+START_TEST(test_init) {
 
   init_leds();
 	fail_unless(digitalRead(ledPin4) == HIGH);
-	fail_unless(digitalRead(ledPin3) == LOW);
+	fail_unless(digitalRead(ledPin3) == HIGH);
+    fail_unless(digitalRead(ledPin2) == HIGH);
+    fail_unless(digitalRead(ledPin1) == HIGH);
 
 }END_TEST
 
-START_TEST(test_loop) {
-
-  run_leds();
-	fail_unless(digitalRead(ledPin4) == LOW);
-	fail_unless(digitalRead(ledPin3) == LOW);
-    fail_unless(digitalRead(ledPin2) == LOW);
-    fail_unless(digitalRead(ledPin1) == LOW);
-
-}END_TEST
-
-
-START_TEST(test_init_two) {
-
+ START_TEST(test_loop_full) {
   init_leds();
-	fail_unless(digitalRead(ledPin4) == HIGH);
-	fail_unless(digitalRead(ledPin2) == LOW);
 
+  display_percentage(100);
+	fail_unless(digitalRead(ledPin1) == HIGH);
+	fail_unless(digitalRead(ledPin2) == HIGH);
+    fail_unless(digitalRead(ledPin3) == HIGH);
+    fail_unless(digitalRead(ledPin4) == HIGH);
 
 }END_TEST
 
-
-START_TEST(test_init_third) {
+START_TEST(test_loop_full2) {
   init_leds();
-  fail_unless(digitalRead(ledPin4) == HIGH);
-  fail_unless(digitalRead(ledPin1) == LOW);
+
+  display_percentage(90);
+	fail_unless(digitalRead(ledPin1) == HIGH);
+	fail_unless(digitalRead(ledPin2) == HIGH);
+    fail_unless(digitalRead(ledPin3) == HIGH);
+    fail_unless(digitalRead(ledPin4) == HIGH);
 
 }END_TEST
+
+
+START_TEST(test_loop_half1) {
+  init_leds();
+
+  display_percentage(80);
+	fail_unless(digitalRead(ledPin1) == LOW);
+	fail_unless(digitalRead(ledPin2) == HIGH);
+    fail_unless(digitalRead(ledPin3) == HIGH);
+    fail_unless(digitalRead(ledPin4) == HIGH);
+
+}END_TEST
+
+
 
 
 Suite * leds_suite(void) {
@@ -64,10 +74,10 @@ Suite * leds_suite(void) {
 
   TCase *tc = tcase_create("Core");
   tcase_add_checked_fixture(tc,setup,teardown);
-  tcase_add_test(tc,test_init_one);
-  tcase_add_test(tc,test_init_two);
-  tcase_add_test(tc,test_init_third);
-  tcase_add_test(tc,test_loop);
+  tcase_add_test(tc,test_init);
+   tcase_add_test(tc,test_loop_full);
+   tcase_add_test(tc,test_loop_full2);
+    tcase_add_test(tc,test_loop_half1);
   tcase_set_timeout(tc,0);
   suite_add_tcase(s,tc);
   return s;
