b9d64366efecfb166f79e33806d0a205a0bc8d26
Jing Liu <gusjingli@student.gu.se>
2012-04-23 11:59:37 +0200

    doxygen added to pressure_check.c.h and hover_api.c

--- src/embedded/Hover/hover_api.c
+++ src/embedded/Hover/hover_api.c
@@ -1,8 +1,11 @@
-/**
- *@file hover_api.c
- *@Author  Eva-Lisa Kedborn, Jing Liu
- *@date 2012-03-28
- *@brief API for controling hovering fan
+/*!
+@file hover_api.c
+@headerfile hover_api.h
+@author Eva-Lisa Kedborn
+@author Jing Liu
+@date 2012-03-28
+@version 0.2
+@brief API for controling hovering fan
  */
 
 #include <stdlib.h>
@@ -14,56 +17,56 @@
 
 #define HOVER_LEVEL 10
 
-/**
- * @brief start the hovring fan
+/*!
+@brief start the hovring fan
  */
 int start_hover() {
   initialize(USING_PIN);
   return 0;
 }
 
-/**
- * @brief stop the fan
+/*!
+@brief stop the hovering fan
  */
 int stop_hover() {          
   hover_func(stop, USING_PIN);
   return 0;
 }
 
-/**
- * @brief minimize the speed
+/*!
+@brief minimize the fan speed
  */
 int hover_min() {
   hover_func(normal, USING_PIN);
   return 0;
 }
 
-/**
- * @brief maximize the speed
+/*!
+@brief maximize the fan speed
  */
 int hover_max() {
   hover_func(turbo, USING_PIN);
   return 0;
 }
 
-/**
- * @brief decrease the speed
+/*!
+@brief decrease the fan speed
  */
 int decrease_hover() {       
   hover_change(decrease, USING_PIN,HOVER_LEVEL);
   return 0;
 }
 
-/**
- * @brief increase the speed
+/*!
+@brief increase the speed
  */
 int increase_hover() {
   hover_change(increase, USING_PIN, HOVER_LEVEL);
   return 0;
 }
 
-/**
- * @brief fan speed cotrolled by pressure sensor automatically
+/*!
+@brief fan speed cotrolled by pressure sensor automatically
  */
 int autocontrol() {       
   start_sensor_reading();
--- src/embedded/Hover/pressure_check.c
+++ src/embedded/Hover/pressure_check.c
@@ -1,8 +1,12 @@
 /* 
-Author: Eva-Lisa Kedborn, Jing Liu
-Date: 2012-03-20
-Description: Loop to continuously read air pressure inside the hovercraft
-             skirt and adjust the speed of the hovering fan accordingly
+@file pressure_check.c
+@headerfile pressure_check.h
+@author Eva-Lisa Kedborn
+@author Jing Liu
+@date: 2012-03-20
+@details continuously read air pressure inside the hovercraft
+         skirt and adjust the speed of the hovering fan accordingly
+@version 0.1
 */
 
 #include <stdio.h>
@@ -13,11 +17,24 @@ Description: Loop to continuously read air pressure inside the hovercraft
 #define MAX_GAP_RANGE 60
 #define MIN_GAP_RANGE 40
 
+
+/*!
+@brief Function to get pressure data from the pressure sensor
+@param takes no parameters
+@returns integer 0 if successful
+*/
 int start_sensor_reading() {
     handle_pressure(get_pressure());
     return 0;
 }
 
+/*!
+@brief Function to evaluate the pressure data
+@param the pressure difference inside and outside the skirt
+@returns integer 1 if the pressure should be increased,
+         interger -1 if the pressure should be decreased,
+	 integer 0 if no adjustment needs to be made
+*/
 int handle_pressure(int gap){
   
   if(gap > MAX_GAP_RANGE){
