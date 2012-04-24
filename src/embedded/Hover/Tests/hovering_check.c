d12380caeac5650d783a79439f6b84f74280bb16
navid amiriarshad <amirinavid83@gmail.com>
2012-04-24 15:59:51 +0200

    writting testcases

--- src/embedded/Hover/Tests/hovering_check.c
+++ src/embedded/Hover/Tests/hovering_check.c
@@ -25,7 +25,8 @@
 @returns
 */
 void setup(void) {
-	ard_init(USING_PIN);
+	init();
+	initialize(USING_PIN, TEST_PIN);
 }
 
 /* Finalize for the tests that initilized the Arduino at setoup */
@@ -37,10 +38,10 @@ void setup(void) {
 void teardown(void) {
 }
 
-/* Id/Title: EN1/Test Arduino board initiation 
-   Purpose: test correct implementation of hoverfan functions
-   Prerequisites: none
-   Expected results: the correct hoverfan function has been called
+/* Id/Title: EN1/Test Motor initiation 
+   Purpose: Test correct implementation of motor initiation functions.
+   Prerequisites: init(); should be called in the main
+   Expected results: The motor should be initiated
    Pass/Fail criteria: When check is run response is 100%: Checks: 4,
                        Failures: 0, Errors: 0/any other results
 */
@@ -49,10 +50,39 @@ void teardown(void) {
 @param
 @returns
 */
-START_TEST(test_ard_init) {
-	/* Define using pin on Arduino board */
-	fail_unless((ard_init(USING_PIN) == 0), "ard init failed");
+START_TEST(test_initialize) {
+	fail_unless((initialize(USING_PIN, TEST_PIN) == 0), "ard init failed");
 }END_TEST
+
+/* Id/Title: EN2/Test pin programming on Arduino board 
+   Purpose: Check the function that gets a value and sets this value on
+   an output pin on Arduino
+   Prerequisites: init(); should be called in the main
+   Expected results: The value should be set on the pin
+   Pass/Fail criteria: When check is run response is 100%: Checks: 4,
+                       Failures: 0, Errors: 0/any other results
+*/
+
+/*!
+@brief
+@param
+@returns
+*/
+START_TEST(test_pin_program) {
+	fail_unless((pin_program(using_pin, TEST_PIN, level) \
+	== 0),"pin test failed\n");
+}END_TEST
+
+/* Id/Title: EN3/Test Check level and fix level 
+   Purpose: Check the function which tests the value sending from the 
+		set_fix_level function to Arduino pin and fix it in the case 
+		of violating boundary values.
+   Prerequisites: init(); ans initi
+   Expected results: Motor shouldn't stop in the case of any power level 
+					that is sent to motor.
+   Pass/Fail criteria: When check is run response is 100%: Checks: 4,
+                       Failures: 0, Errors: 0/any other results
+/////////////////////////////////////////////////////////////////
 
 /*!
 @brief
@@ -60,55 +90,84 @@ START_TEST(test_ard_init) {
 @returns
 */
 START_TEST(test_check_and_fix_level) {
-	/* A random number between 255 and 1255 which is higher than \
+	/* A random value between 255 and 1255 which is higher than \
 	legal range*/
 	int throttle_illegel_high_level = random(1000) +255;
-	/* A random number between 133 and 254 which is within the legal range*/
+	/* A random value between 133 and 254 which is within the legal range*/
 	int throttle_illegel_ok_level = random(111) +133;
-	/* A random number between 0 and 132 which is lower than legal range*/
+	/* A random value between 0 and 132 which is lower than legal range*/
 	int throttle_illegel_low_level = randon(132);
-	fail_unless((check_and_fix_level(USING_PIN, throttle_illegel_high_level) \
-	== HIGHEST_LEVEL),"pin test failed\n");
-	fail_unless((check_and_fix_level(USING_PIN,throttle_ok_level) \
-	== throttle_ok_level),"pin test failed\n");
-	fail_unless((check_and_fix_level(USING_PIN,throttle_illegel_low_level) \
-	== LOWEST_LEVEL),"pin test failed\n");
+	fail_unless((check_and_fix_level(USING_PIN, TEST_PIN \
+	throttle_illegel_high_level) == HIGHEST_LEVEL),"pin test failed\n");
+	fail_unless((check_and_fix_level(USING_PIN, TEST_PIN, 
+	throttle_ok_level) == throttle_ok_level),"pin test failed\n");
+	fail_unless((check_and_fix_level(USING_PIN, TEST_PIN, 
+	throttle_illegel_low_level) == LOWEST_LEVEL),"pin test failed\n");
 }END_TEST
+
+
+
+/* Id/Title: EN4/Test Check level and fix level for increase 
+   Purpose: Check the function which tests the value sending from increase 
+			function
+		to Arduino pin and fix it in the case of violating boundary values.
+   Prerequisites: init(); ans initi
+   Expected results: Motor shouldn't stop in the case of any power level 
+					that is sent to motor.
+   Pass/Fail criteria: When check is run response is 100%: Checks: 4,
+                       Failures: 0, Errors: 0/any other results
+/////////////////////////////////////////////////////////////////
 
 /*!
 @brief
 @param
 @returns
 */
-START_TEST(test_pin_program) {
-	fail_unless((pin_program(using_pin, level) == 0),"pin test failed\n");
+START_TEST(test_check_and_fix_level) {
+	/* A random value between 255 and 1255 which is higher than \
+	legal range*/
+	int throttle_illegel_high_level = random(1000) +255;
+	/* A random value between 133 and 254 which is within the legal range*/
+	int throttle_illegel_ok_level = random(111) +133;
+	/* A random value between 0 and 132 which is lower than legal range*/
+	int throttle_illegel_low_level = randon(132);
+	fail_unless((check_and_fix_level(USING_PIN, TEST_PIN \
+	throttle_illegel_high_level) == HIGHEST_LEVEL),"pin test failed\n");
+	fail_unless((check_and_fix_level(USING_PIN, TEST_PIN, 
+	throttle_ok_level) == throttle_ok_level),"pin test failed\n");
+	fail_unless((check_and_fix_level(USING_PIN, TEST_PIN, 
+	throttle_illegel_low_level) == LOWEST_LEVEL),"pin test failed\n");
 }END_TEST
+
+/* Id/Title: EN5/Test Check level and fix level 
+   Purpose: Check the function which tests the value sending from API 
+		to Arduino pin and fix it in the case of violating boundary values.
+   Prerequisites: init(); ans initi
+   Expected results: Motor shouldn't stop in the case of any power level 
+					that is sent to motor.
+   Pass/Fail criteria: When check is run response is 100%: Checks: 4,
+                       Failures: 0, Errors: 0/any other results
+/////////////////////////////////////////////////////////////////
 
 /*!
 @brief
 @param
 @returns
 */
-START_TEST(test_pin_program) {
-	fail_unless((pin_program(using_pin,level) == 0), "pin test failed\n");
-}END_TEST
-
-/*!
-@brief
-@param
-@returns
-*/
-START_TEST(test_pin_program) {
-	fail_unless((pin_program(using_pin, level) == 0), "pin test failed\n");
-}END_TEST
-
-/*!
-@brief
-@param
-@returns
-*/
-START_TEST(test_pin_program) {
-	fail_unless((pin_program(using_pin, level) == 0), "pin test failed\n");
+START_TEST(test_check_and_fix_level) {
+	/* A random value between 255 and 1255 which is higher than \
+	legal range*/
+	int throttle_illegel_high_level = random(1000) +255;
+	/* A random value between 133 and 254 which is within the legal range*/
+	int throttle_illegel_ok_level = random(111) +133;
+	/* A random value between 0 and 132 which is lower than legal range*/
+	int throttle_illegel_low_level = randon(132);
+	fail_unless((check_and_fix_level(USING_PIN, TEST_PIN \
+	throttle_illegel_high_level) == HIGHEST_LEVEL),"pin test failed\n");
+	fail_unless((check_and_fix_level(USING_PIN, TEST_PIN, 
+	throttle_ok_level) == throttle_ok_level),"pin test failed\n");
+	fail_unless((check_and_fix_level(USING_PIN, TEST_PIN, 
+	throttle_illegel_low_level) == LOWEST_LEVEL),"pin test failed\n");
 }END_TEST
 
 
