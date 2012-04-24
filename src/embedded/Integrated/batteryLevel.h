cf633cf0db7b7b063663198ea1267a6ba1f0ef19
Nahid Vafaie <nahid@student.gu.se>
2012-04-24 20:01:06 +0200

    some changes in header file.

--- src/embedded/Integrated/batteryLevel.h
+++ src/embedded/Integrated/batteryLevel.h
@@ -1,19 +1,20 @@
 /*
- * @ Module name:  fourLedsOn.h
- * @ Description:  Header file for fourLedsOn.c
- * @ Author names :Nahid Vafaie
- * @ Release      :13 April 2012
- * @ Version      : 2
+ * @ Module name  :batteryLevel.h
+ * @ Description  :Header file for batteryLevel.c
+ * @ Author names :Nahid Vafaie,Kuhan Loh, Aran Gerami, Denir Leric
+ * @ Release      :24 April 2012
+ * @ Version      :3
  * @ Refrences    : Arduino.cc, sandklef.com
  */
 
-#ifndef FOUR_LEDS_ON_H
-#define FOUR_LEDS_ON_H
+#ifndef BATTERY_LEVEL_H
+#define BATTERY_LEVEL_H
 #define ledPin1 2
 #define ledPin2  4
 #define ledPin3  7
 #define ledPin4  8
 #define timer1  500
+
 
 
 void init_leds(void);
