342ca502fbfe76b8e75264265632f0baece3b9f9
Seyed Ehsan Mohajerani <gusmohase@student.gu.se>
2012-04-30 08:35:05 +0200

    comment on audible_test.c and hovering_control.c

--- src/embedded/Hover/hovering_control.c
+++ src/embedded/Hover/hovering_control.c
@@ -1,35 +1,54 @@
-/*
- * @ Module name:  hovering_control.c
- * @ Description:  Contains the functions that control
- *   the hovering fan speed.
+
+/***************************************************************************
+ * Copyright (C) 2012  Seyed Ehsan Mohajerani
+ *                     Navid Amiriarshad
+ * 
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @ Author names : Seyed Ehsan Mohajerani, Navid Amiriarshad
- * @ Release      : 20 March 2012
- * @ Version      : 1.0
- * @ References    : Arduino.cc, sandklef.com
- * dreamincode.net/forums/topic/34861-functions-stored-in-structure
- * Turnigy_Plush_and_Sentry_ESC user manual
+ ****************************************************************************/
+
+/*!
+ @file hovering_control.c
+ @headerfile hovering_control.h
+ @brief This module contains the functions that control the hovering fan speed
+ @author Seyed Ehsan Mohajerani
+ @author Navid Amiriarshad
+ @date 20 March 2012
+ @version 1.0
+ @refrence Arduino.cc
+ @refrence sandklef.com
+ @refrence hoveritu.com
+ @refrence dreamincode.net/forums/topic/34861-functions-stored-in-structure
+ @refrence Turnigy_Plush_and_Sentry_ESC user manual
+ @refrence for Coding standard ece.cmu.edu/~eno/coding/CCodingStandard.html
+ @refrence for commenting stack.nl/~dimitri/doxygen/commands.html#cmdparam
  */
 
-/* Includes */
 #include <Arduino.h> /* Arduino header */
 #include <hovering_motor.h>
 #include <hovering_control.h>
 #include <hovering_init_fix.h>
 #include <print_audible_test.h>
 
-/* Definition */
 /* The minimum PWM value that is possible to send to ESC from Arduino pin
  and it is for initiating the motor */
 #define THROTTLE_STICK_BOTTOM_LEVEL 100
 
-/* Variable */
 /* The PWM value which is sent to ESC from Arduino and represents the motor
  rotating power it can be between 143 (for lowest rotating speed) and 254
 (for the highest rotating speed */
 int throttle_stick_level = 0;
-
-/* Functions */
 
 /* Start the motor */
 int start(int using_pin, int test_pin) {
@@ -41,7 +60,7 @@ int start(int using_pin, int test_pin) {
 	test_start (test_pin);
 	return 0;
 }
-
+0
 /* Set the level to 100 or start the motor \
 if the motor has not been started (The minimum acceptable value for ESC) */
 int throttle_stick_bottom(int using_pin, int test_pin) {
