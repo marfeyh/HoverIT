074fee8e619e06dfa9f7e6134a4f1fa6cb64a049
gusezepr@student.gu.se <anthonyezeh@ubuntu.ubuntu-domain>
2012-05-01 18:16:09 +0200

    added comment to header file section

--- src/embedded/brake_propulsion/brake_propulsion.h
+++ src/embedded/brake_propulsion/brake_propulsion.h
@@ -1,3 +1,19 @@
+/*!
+ * @file brake_propulsion.h
+ * @brief This is just the headerfile for brake_propulsion.c
+ * @author: Ezeh Prince Anthony Anayo
+ * @author: Xinran He
+ * @version: 0.2
+ * @date: 2012-04-26
+ * @reference: Arduino.cc
+ * @reference: sandklef.com
+ * @reference: hoveritu.com
+ */
+
+#ifnedef BRAKE_PROPULSION_H
+#define  BRAKE_PROPULSION_H
 
 int brake_pro(void);
-int reverse_motor(int current_hover_speed);
+int reverse_prop_motor(int current_hover_speed);
+
+#endif
