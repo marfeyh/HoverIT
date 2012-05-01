f259f4644588a75f5f965b20cb8eb91b2ad497ad
gusezepr@student.gu.se <anthonyezeh@ubuntu.ubuntu-domain>
2012-05-01 20:37:52 +0200

    added licence and brief code

--- src/embedded/brake_propulsion/brake_propulsion.h
+++ src/embedded/brake_propulsion/brake_propulsion.h
@@ -1,3 +1,22 @@
+/***************************************************************************
+ * Copyright (C) 2012  Ezeh Prince Anthony Anayo
+ *                     Xinran He
+ * 
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************************/
+ 
 /*!
  * @file brake_propulsion.h
  * @brief This is just the headerfile for brake_propulsion.c
@@ -11,8 +30,10 @@
  */
 
 #ifndef BRAKE_PROPULSION_H
-#define  BRAKE_PROPULSION_H
+#define BRAKE_PROPULSION_H
+#define DELAY_SPEED_TIME 2000 milliseconds
 
+int int_brake_propulsion(void);
 int brake_pro(void);
 int reverse_prop_motor(int current_hover_speed);
 
