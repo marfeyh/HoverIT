48134534a2896df945875c9c311a71d8d0d00bc3
gusezepr@student.gu.se <anthonyezeh@ubuntu.ubuntu-domain>
2012-05-03 00:08:22 +0200

    commented the header section

--- src/embedded/brake_propulsion/tests/check_main.c
+++ src/embedded/brake_propulsion/tests/check_main.c
@@ -1,8 +1,11 @@
 /*!
- *@authors: Xinran He, Ezeh Prince Anthony Anayo
- *@description: The main function for the unit tests of braking and reversing 
+ * @author: Xinran He
+ * @author: Ezeh Prince Anthony Anayo
+ * @description: The main function for the unit tests of braking and reversing 
    the direction of the propulsion fan
- *@date: 2012-05-02
+ * @date: 2012-05-02
+ * @version: 0.2
+ * @reference:http://check.sourceforge.net/doc/check_html/index.html#SEC_Top
  */
  
 #include <stdlib.h>
