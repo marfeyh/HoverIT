6449bc9f64041521b21a6802bf5cf6ca21b5cdb6
Rob Bruinsma <gusbruiro@student.gu.se>
2012-05-02 15:45:07 +0200

    Added Copyright / License

--- src/embedded/Pressure/pressure.c
+++ src/embedded/Pressure/pressure.c
@@ -1,12 +1,26 @@
 /*! 
- *  \file    pressure.c
- *  \details Gets voltage ranges from inner and outer pressure sensors,
- *           converts those to Pa and calculates the difference.
- *  \author  Rob Bruinsma
- *  \author  Anna Orazova
- *  \author  Markus Feyh
- *  \version 2.1
- *  \date    2012/05/02
+ *  \file	pressure.c
+ *  \details	Gets voltage ranges from inner and outer pressure sensors,
+ *		converts those to Pa and calculates the difference.
+ *  \author	Rob Bruinsma
+ *  \author	Anna Orazova
+ *  \author	Markus Feyh
+ *  \version	2.1
+ *  \date	2012/05/02
+ *  \copyright	Copyright (C) 2012  Kappa@HoverIT
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*!
