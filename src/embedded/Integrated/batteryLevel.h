aeef55d45c8138389a0757e154036d490ae0924b
Denir Leric <denir.leric@gmail.com>
2012-05-03 09:49:04 +0200

    Removed unused code in batteryLevel.h

--- src/embedded/Integrated/batteryLevel.h
+++ src/embedded/Integrated/batteryLevel.h
@@ -9,11 +9,6 @@
 
 #ifndef BATTERY_LEVEL_H
 #define BATTERY_LEVEL_H
-//#define ledPin1 2
-//#define ledPin2  4
-//#define ledPin3  7
-//#define ledPin4  8
-//#define timer1  500
 #define SENSOR_PIN 5
 
 uint8_t check(void);
