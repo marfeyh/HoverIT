8dd532a8524fdb67184b42bc1ec963bdda952655
Rob Bruinsma <gusbruiro@student.gu.se>
2012-05-04 15:34:07 +0200

    Commented out unused fuctions, fixed return types

--- src/embedded/Pressure/pressure.h
+++ src/embedded/Pressure/pressure.h
@@ -26,30 +26,25 @@
 #define PRESSURE_H
 
 /*!
- * /brief     Function for getting pressure difference
- *  
- * /return    Difference between inside and outside pressure in Pa
- */
-int get_pressure_difference(void);
-
-/*!
  * /brief     Function for getting the inner pressure sensor's value
  *  
- * /return    Inside pressure in Pa
+ * /return    Inside pressure in kPa
  */
-int get_pressure_inner(void);
+//float get_pressure_inner(void); Not currently in use
 
 /*!
  * /brief     Function for getting the outer pressure sensor's value
  *  
- * /return    Outer pressure in Pa
+ * /return    Outer pressure in kPa
  */
 
-int get_pressure_outer(void);
+//float get_pressure_outer(void); Not currently in use
 
 /*!
- * /brief     Function for getting pressure and pressure difference from physical sensors
+ * /brief     Function to get pressure values and calculates the difference
+ *
+ * /return    Pressure difference
  */
-void check_pressure(void);
+unsigned int check_pressure(void);
 
 #endif
