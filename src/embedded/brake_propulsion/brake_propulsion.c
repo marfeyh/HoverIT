a22dc1c49d641f035b4628113874d2013edf9d42
gusezepr@student.gu.se <anthonyezeh@ubuntu.ubuntu-domain>
2012-05-09 23:56:47 +0200

    committing the modified changes

--- src/embedded/brake_propulsion/brake_propulsion.c
+++ src/embedded/brake_propulsion/brake_propulsion.c
@@ -6,35 +6,15 @@
 #include <pin.h>
 #include <searduino.h>
 
-/*!
- * @brief: reverse rotation 
- */
-int reverse_prop_motor(){
-<<<<<<< HEAD
-   digitalWrite(RelayPin, HIGH);
-   delay(2000);
-   analogWrite(PIN, 100);
-   delay(2000);
-   digitalWrite(RelayPin, LOW);
-   delay(2000);
-   analogWrite(PIN, 100);
-   delay(2000);
-   change_pro_speed(MAXSPEED);
-   brake_pro();
-   return 0;
-}
-
 
 /*!
  * @brief: get hovercraft speed. If the speed is less than
  * 10, then the propulsion fan is stopped.
  */
 int brake_pro(void){
-=======
    int hover_speed = 0;
    digitalWrite(RELAYPIN,LOW);
    set_propulsion_fan(MAXSPEED);
->>>>>>> fc3408bf60875ec5fe7784822b865277a78e2728
   for(;;){
     hover_speed = get_hover_speed();
     if(hover_speed < 10){ //need test
