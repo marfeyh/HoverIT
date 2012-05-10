40417efc683a5d228aebd39a79a4aa75f4fc0fd5
gusezepr@student.gu.se <anthonyezeh@ubuntu.ubuntu-domain>
2012-05-11 00:12:39 +0200

    added comment to check_propulsion

--- src/embedded/brake_propulsion/tests/check_propulsion.h
+++ src/embedded/brake_propulsion/tests/check_propulsion.h
@@ -1,3 +1,11 @@
+/*!
+@file check_propulsion.h
+@author Ezeh Prince Anthony Anayo
+@author Xinran He
+@brief Header file for testing the control of propulsion fan files.
+@version 0.2
+@date 2012-05-10
+*/
 #ifndef CHECK_PROPULSION_H
 #define CHECK_PROPULSION_H
 
