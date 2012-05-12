5b5e54cff0ff6e912cd17383e51b08c2048c061b
gusezepr@student.gu.se <anthonyezeh@ubuntu.ubuntu-domain>
2012-05-12 17:50:05 +0200

    added licence & comment and refactored hover_speed_stub

--- src/embedded/Propulsion/hover_speed_stub.h
+++ src/embedded/Propulsion/hover_speed_stub.h
@@ -1,2 +1,47 @@
+/***************************************************************************
+ * Copyright (C) 2012  Ezeh Prince Anthony Anayo
+ *                     Xinran He
+ * 
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************************/
+ 
+ /*! 
+ @file hover_speed_stub.h
+ @brief This headerfile contains function for receieving the assumed speed value
+ @author Ezeh Prince Anthony Anayo
+ @author Xinran He
+ @date 10 May 2012
+ @version 3.0
+ @reference Arduino.cc
+ @reference sandklef.com
+ @reference hoveritu.com
+ @reference dreamincode.net/forums/topic/34861-functions-stored-in-structure
+ @reference Turnigy_Plush_and_Sentry_ESC user manual
+ @reference for Coding standard ece.cmu.edu/~eno/coding/CCodingStandard.html
+ @reference for commenting stack.nl/~dimitri/doxygen/commands.html#cmdparam
+ */ 
+ 
+#ifndef HOVER_SPEED_STUB_H
+#define HOVER_SPEED_STUB_H
 
+/*!
+ @brief Function to fetch the current hovering speed
+ @param takes no parameter
+ @returns the current hovering speed
+*/ 
 int get_hover_speed();
+
+#endif
+
